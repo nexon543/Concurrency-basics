b4bbd092181b403f23d9a5e0ca40f754af46df55
Hleb Aksionau <glebaks94@gmail.com>
2018-02-13 14:41:13 +0300

    SimpleDeadLoack

--- src/dead_lock/DeadLock.java
+++ src/dead_lock/DeadLock.java
@@ -3,17 +3,45 @@ public class DeadLock {
 	
 	
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		
 		String str1="one";
 		String str2="two";
 		new Thread (()-> {
 			synchronized (str1) {
-				Thread.sleep(1000);
-				
-				
+				System.out.println("Thread 1 started");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						
+						e.printStackTrace();
+					}
+					synchronized (str2) {
+						for (int i=0; i< 1000;i++) {
+							System.out.println("Thread 1 access to resource 2. Iteration:"+i);
+							}
+					}
 			}
 		}).start();
-	}
 
+		
+		new Thread (()-> {
+			synchronized(str2) {
+				System.out.println("Thread 2 started");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				synchronized (str1) {
+					for (int i=0; i< 1000;i++) {
+					System.out.println("Thread 2 access to resource 1. Iteration:"+i);
+					}
+				
+			}
+			}
+		}).start();
+		
+		}
+	
 }
